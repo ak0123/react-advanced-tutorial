438ec89243e8d4be944921bb0c931e160c68bd3f
z0q1 <z0q1@z0q1s-MBP.fios-router.home>
2024-02-17 23:28:30 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react'
+
+function reducer({state}){
+  switch(state){
+    case 'PRESSED_ONCE':
+      return {
+        state: 'PRESSED_TWO'
+      }
+    case 'PRESSED_TWO':
+      return {
+        state: 'PRESSED_THREE'
+      }
+    case 'PRESSED_THREE':
+      return {
+        state: 'PRESSED_ONCE'
+      }
+  }
+}
+
+const Button = (props) => {
+  const [counter, setCounter] = useState(0)
+
+  const [state, dispatch] = useReducer(reducer, { 
+    state: 'PRESSED_ONCE'
+  })
+  return (
+    <div
+      style={{
+        color: props.color,
+        textDecoration: props.underline ? 'underline' : undefined
+      }}
+      onClick={() => setCounter((c) => c + props.increment)}
+    >
+      <div onClick={() => dispatch()}>I am a button</div>
+      <div>{state.state}</div>
+
+    </div>
+  )
+}
 
 function App() {
+  const props = {
+    increment: 2,
+    underline: true,
+    color: 'black'
+  }
+
+  //  jsx code -> React.createElement('Button)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button {...props} color="blue"/>
+      <Button {...props} color="black"/>
+      <Button {...props} color="green"/>
     </div>
   );
 }
